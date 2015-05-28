da1b23f1cdd9a0e66f64cc74898265e6d51d2f1a
AJ Friend <ajfriend@gmail.com>
2015-05-28 00:48:29 -0700

    workspace object sorta working, making the solver method

--- cython/cyscs.pyx
+++ cython/cyscs.pyx
@@ -1,11 +1,21 @@
 # use the python malloc/free to have the memory attributed to python.
 #from cpython.mem cimport PyMem_Malloc, PyMem_Free
 
+# QUESTION: why do i get segfaults when compiling on OSX?
+
 import numpy as np
 cimport numpy as np
 
-# just don't import scipy.sparse
-#import scipy.sparse as sp
+stg_default = dict(normalize = 1,
+                   scale = 1,
+                   rho_x = 1e-3,
+                   max_iters = 2500,
+                   eps = 1e-3,
+                   alpha = 1.5,
+                   cg_rate = 2,
+                   verbose = 1,
+                   warm_start = 0)
+
 
 cpdef hello():
     print "hello"
@@ -22,57 +32,20 @@ cpdef howdy(np.ndarray[scs_float] a, np.ndarray[scs_int] b):
     print_numeric_type(<scs_float*> a.data, b[0])
 
 
-cdef class Workspace:
-    pass
-#    cdef Work * _work
-    #cdef Settings _settings
-#    cdef Info _info
-#    cdef AMatrix _A
-    #cdef Data _data
-#    cdef Cone _cone
+# QUESTION: why can't i make settings a kwargs: **settings (i get a segfault)
+# QUESTION: why do i get a segfault if i use "def" instead of "cpdef"?
+cpdef myscs_solve(data, workspace=None, sol=None, settings=None):
+    if settings is None:
+        settings = {}
 
-    #def __cinit__(self, data, **settings):
-    #    pass
+    if workspace is None:
+        workspace = Workspace(data, **settings)
 
-#        # this is a copy of the dict to the settings struct
-#        self._settings = stg_default.copy().update(settings)
-
-#        A = data['A']
-#        m,n = A.shape
-
-#        # TODO: do we always need this conversion?
-#        # check what scs_int is?
-#        A.indices = A.indices.astype(np.int64)
-#        A.indptr = A.indptr.astype(np.int64)
-
-#        self._A = make_amatrix(A.data, A.indices, A.indptr, m, n)
-
-#        # TODO: double check that this isn't a copy.
-#        cdef np.ndarray[double] b = data['b']
-#        cdef np.ndarray[double] c = data['c']
-
-#        self._data = Data(m, n, &self._A, <scs_float*>b.data, <scs_float*>c.data, &self._settings)
-#        self._cone = make_cone(data['cones'])
-
-#        self._work = scs_init(&self._data, &self._cone, &self._info)
-#        #cdef Work * scs_init(const Data * d, const Cone * k, Info * info)
-
-#        if self._work == NULL: 
-#            raise MemoryError("Memory error in allocating Workspace.")
+    # *update* the settings dict
+    workspace.settings = settings
 
 
-
-#def myscs_init(data, **settings):
-#    # data is a dict with keys A,b,c, cones
-#    # info is a dict. the set up function just needs it to write down the setuptime
-#    #cdef Work * scs_init(const Data * d, const Cone * k, Info * info)
-#    workspace = None
-#    return workspace
-#    # work contains a pointer to settings, let the user access
-
-#def myscs_solve(data, workspace=None, sol=None, **settings):
-#    if workspace is None:
-#        workspace = myscs_init(data, **settings)
+    #workspace.set_settings(settings)
 #    # sol is a dict of numpy arrays
 #    # if none, make the numpy arras yourself
 
@@ -87,116 +60,137 @@ cdef class Workspace:
 #    return sol, workspace
 
 
-#def myscs(data, cone, **settings):
-#     #only setting missing here is warm start
-#    _A = data['A']
-#    m,n = _A.shape
+cdef class Workspace:
+    cdef:
+        Work * _work
+        Settings _settings
+        Info _info
+        AMatrix _A
+        # maybe it doesn't really make sense to have Workspace keep the _data or the cone
+        Data _data
+        Cone _cone
 
-#    # TODO: do we always need this conversion?
-#    # check what scs_int is?
-#    _A.indices = _A.indices.astype(np.int64)
-#    _A.indptr = _A.indptr.astype(np.int64)
+    property settings:
+        def __get__(self):
+            return <dict>self._settings
+        def __set__(self, rhs):
+            """ Not really a setter. Actually just updates the internal Settings
+            struct/dict. Unknown keys are ignored.
 
-#    cdef AMatrix A = make_amatrix(_A.data, _A.indices, _A.indptr, m, n)
+            For example,
 
-#    stgs = stg_default.copy()
-#    stgs.update(settings)
+            workspace.settings = dict(apples=-17, max_iter=137)
 
-#    cdef Settings csettings = stgs
+            would leave the current settings unchanged, except for max_iter,
+            which is updated. The 'apples' key is ignored.
+            """
+            # need to cdef stgs as a dict so that the struct is converted to a dict
+            cdef dict stgs = self._settings
+            stgs.update(rhs)
+            self._settings = stgs
 
-#    cdef np.ndarray[double] b = data['b']
-#    cdef np.ndarray[double] c = data['c']
-
-#    cdef Data cdata = Data(m, n, &A, <scs_float*>b.data, <scs_float*>c.data, &csettings)
-
-#    cdef Info info # doesn't need to be initialized
-
-#    x = np.zeros(n)
-#    y = np.zeros(m)
-#    s = np.zeros(m)
-#    cdef Sol sol = make_sol(x, y, s)
-
-#    cdef Cone ccone = make_cone(cone)
-
-#    cdef scs_int result = scs(&cdata, &ccone, &sol, &info)
-
-#    return {'x':x, 'y':y, 's':s, 'info':info, 'settings':csettings}
+    property info:
+        def __get__(self):
+            return <dict>self._info
 
 
+    # need to make a dealloc method
+    # make sure we aren't freeing anythign else when we free work
 
-#stg_default = dict(normalize = 1,
-#                   scale = 1,
-#                   rho_x = 1e-3,
-#                   max_iters = 2500,
-#                   eps = 1e-3,
-#                   alpha = 1.5,
-#                   cg_rate = 2,
-#                   verbose = 1,
-#                   warm_start = 0)
+    def __cinit__(self, data, **settings):
+        stgs = stg_default.copy()
+        if settings:
+            stgs.update(settings)
 
-#def mytest3():
+        self._settings = stgs
 
-#    ij = np.array([[0,1,2,3],[0,1,2,3]])
-#    A = sp.csc_matrix(([-1.,-1.,1.,1.], ij), (4,4))
-#    cdef np.ndarray[double] b = np.array([0.,0.,1,1])
-#    cdef np.ndarray[double] c = np.array([1.,1.,-1,-1])
-#    cone = {'l':4}
+        A = data['A']
+        cdef scs_int m, n
+        m,n = A.shape
 
-#    data = dict(A=A, b=b, c=c)
+        # TODO: do we always need this conversion?
+        # check what scs_int is?
+        #A.indices = A.indices.astype(np.int64)
+        #A.indptr = A.indptr.astype(np.int64)
 
-#    return myscs(data, cone)
+        self._A = make_amatrix(A.data, A.indices, A.indptr, m, n)
+
+        # QUESTION: if i put data[b] in the call below, i get an error, why?
+        cdef np.ndarray[scs_float] b = data['b']
+        cdef np.ndarray[scs_float] c = data['c']
+
+        self._data = Data(m, n, &self._A, <scs_float*>b.data, <scs_float*>c.data, &self._settings)
+        self._cone = make_cone(data['cones'])
+
+        self._work = scs_init(&self._data, &self._cone, &self._info)
+
+        if self._work == NULL: 
+            raise MemoryError("Memory error in allocating Workspace.")
+
+    def show(self):
+        print "showing data"
+        print self._settings
+        print self._info
+        #print self._data.stgs
+        cdef scs_int m = self._data.m
+        cdef scs_float * b = self._data.b
+
+        # QUESTION: why do i get segfaults whenever i try to use memoryviews?
+
+        for i in range(m):
+            print b[i]
 
 
 ## this first version messes up everything, for some reason
 ##cdef Sol make_sol(x, y, s):
-#cdef Sol make_sol(np.ndarray[scs_float] x, np.ndarray[scs_float] y, np.ndarray[scs_float] s):
-#    cdef Sol sol = Sol(<scs_float*>x.data, <scs_float*>y.data, <scs_float*>s.data)
-#    #cdef Sol sol = Sol(NULL, NULL, NULL)
-#    return sol
+cdef Sol make_sol(np.ndarray[scs_float] x, np.ndarray[scs_float] y, np.ndarray[scs_float] s):
+    cdef Sol sol = Sol(<scs_float*>x.data, <scs_float*>y.data, <scs_float*>s.data)
+    #cdef Sol sol = Sol(NULL, NULL, NULL)
+    return sol
 
-#cdef AMatrix make_amatrix(np.ndarray[scs_float] data, np.ndarray[scs_int] ind, np.ndarray[scs_int] indptr, int m, int n):
-#    # Amatrix is not really big, so there's no need to dynamically allocate it.
-#    # difference with C/python? don't need to make this dynamically declared?
-#    # maybe fill a local array and then memcopy to dynamically allocated array
-#    cdef AMatrix cA = AMatrix(<scs_float*>data.data, <scs_int*>ind.data, <scs_int*>indptr.data, m, n)
-#    return cA
+cdef AMatrix make_amatrix(np.ndarray[scs_float] data, np.ndarray[scs_int] ind, np.ndarray[scs_int] indptr, scs_int m, scs_int n):
+    # Amatrix is not really big, so there's no need to dynamically allocate it.
+    # difference with C/python? don't need to make this dynamically declared?
+    # maybe fill a local array and then memcopy to dynamically allocated array
+    cdef AMatrix cA = AMatrix(<scs_float*>data.data, <scs_int*>ind.data, <scs_int*>indptr.data, m, n)
+    return cA
 
-#cdef Cone make_cone(pycone):
-#    # maybe we should be wrapping cone in a python object to manage memory and deallocation
-#    cdef np.ndarray[scs_int] q = None
-#    cdef np.ndarray[scs_int] s = None
-#    cdef np.ndarray[scs_float] p = None
+cdef Cone make_cone(pycone):
+    # maybe we should be wrapping cone in a python object to manage memory and deallocation
+    cdef np.ndarray[scs_int] q = None
+    cdef np.ndarray[scs_int] s = None
+    cdef np.ndarray[scs_float] p = None
 
-#    cdef Cone ccone = Cone(f=0,l=0,q=NULL,qsize=0,s=NULL,ssize=0,ep=0,ed=0,psize=0,p=NULL)
-#    if 'f' in pycone:
-#        ccone.f = pycone['f']
-#    if 'l' in pycone:
-#        ccone.l = pycone['l']
-#    if 'q' in pycone:
-#        # todo: careful with the integer types here
-#        q = np.array(pycone['q'], dtype=np.int64)
-#        ccone.q = <scs_int*>q.data
-#        ccone.qsize = len(q)
-#    if 's' in pycone:
-#        # todo: careful with the integer types here
-#        s = np.array(pycone['s'], dtype=np.int64)
-#        ccone.s = <scs_int*>s.data
-#        ccone.ssize = len(s)
-#    if 'ep' in pycone:
-#        ccone.ep = pycone['ep']
-#    if 'ed' in pycone:
-#        ccone.ed = pycone['ed']
-#    if 'p' in pycone:
-#        # todo: careful with the integer types here
-#        p = np.array(pycone['p'], dtype=np.float64)
-#        ccone.p = <scs_float*>p.data
-#        ccone.psize = len(p)
+    cdef Cone ccone = Cone(f=0,l=0,q=NULL,qsize=0,s=NULL,ssize=0,ep=0,ed=0,psize=0,p=NULL)
+    if 'f' in pycone:
+        ccone.f = pycone['f']
+    if 'l' in pycone:
+        ccone.l = pycone['l']
+    if 'q' in pycone:
+        # todo: careful with the integer types here
+        q = np.array(pycone['q'], dtype=np.int64)
+        ccone.q = <scs_int*>q.data
+        ccone.qsize = len(q)
+    if 's' in pycone:
+        # todo: careful with the integer types here
+        s = np.array(pycone['s'], dtype=np.int64)
+        ccone.s = <scs_int*>s.data
+        ccone.ssize = len(s)
+    if 'ep' in pycone:
+        ccone.ep = pycone['ep']
+    if 'ed' in pycone:
+        ccone.ed = pycone['ed']
+    if 'p' in pycone:
+        # todo: careful with the integer types here
+        p = np.array(pycone['p'], dtype=np.float64)
+        ccone.p = <scs_float*>p.data
+        ccone.psize = len(p)
 
-#    # return the allocated numpy arrays so that they don't get garbage collected
-#    # TODO!! : worry about the numpy arrays here getting garbage collected
+    # return the allocated numpy arrays so that they don't get garbage collected
+    # TODO!! : worry about the numpy arrays here getting garbage collected
 
-#    # maybe we should just make sure to allocate and free this memory
-#    return ccone
+    # maybe we should just make sure to allocate and free this memory
+    return ccone
 
 
 
--- python/ajtest.py
+++ python/ajtest.py
@@ -1,6 +1,7 @@
 import cyscs
 
 import numpy as np
+import scipy.sparse as sp
 
 a = np.random.rand(4)
 b = np.array([1,2,3,4])
@@ -27,3 +28,63 @@ try:
 except Exception as e:
     print "should be something like: Buffer dtype mismatch, expected 'scs_int' but got 'short'"
     print e
+
+
+def mytest3():
+    ij = np.array([[0,1,2,3],[0,1,2,3]])
+    A = sp.csc_matrix(([-1.,-1.,1.,1.], ij), (4,4))
+    A.indices = A.indices.astype(np.int64)
+    A.indptr = A.indptr.astype(np.int64)
+    b = np.array([0.,0.,1,1])
+    c = np.array([1.,1.,-1,-1])
+    cone = {'l':4}
+
+    data = dict(A=A, b=b, c=c, cones=cone)
+
+    return cyscs.Workspace(data, scale=2), b
+
+
+
+w, b = mytest3()
+w.show()
+b[2] = 13
+w.show()
+
+w.settings = {'max_iters':4}
+print w.settings
+
+
+# def myscs(data, cone, **settings):
+#     #only setting missing here is warm start
+#    _A = data['A']
+#    m,n = _A.shape
+
+   # # TODO: do we always need this conversion?
+   # # check what scs_int is?
+   # _A.indices = _A.indices.astype(np.int64)
+   # _A.indptr = _A.indptr.astype(np.int64)
+
+   # cdef AMatrix A = make_amatrix(_A.data, _A.indices, _A.indptr, m, n)
+
+   # stgs = stg_default.copy()
+   # stgs.update(settings)
+
+   # cdef Settings csettings = stgs
+
+   # cdef np.ndarray[double] b = data['b']
+   # cdef np.ndarray[double] c = data['c']
+
+   # cdef Data cdata = Data(m, n, &A, <scs_float*>b.data, <scs_float*>c.data, &csettings)
+
+   # cdef Info info # doesn't need to be initialized
+
+   # x = np.zeros(n)
+   # y = np.zeros(m)
+   # s = np.zeros(m)
+   # cdef Sol sol = make_sol(x, y, s)
+
+   # cdef Cone ccone = make_cone(cone)
+
+   # cdef scs_int result = scs(&cdata, &ccone, &sol, &info)
+
+   # return {'x':x, 'y':y, 's':s, 'info':info, 'settings':csettings}
