f9d83c269e3179bc571fe439d70d1652caf2af1c
AJ Friend <ajfriend@gmail.com>
2015-09-14 13:48:09 -0700

    playing with the scs install script

--- python/setup.py
+++ python/setup.py
@@ -81,43 +81,6 @@ USE_OPENMP = False
 # set to true if linking against blas/lapack libraries that use longs instead of ints for indices:
 USE_64_BIT_BLAS = False
 
-BLAS_LAPACK_LIB_PATHS='BLAS_LAPACK_LIB_PATHS'
-BLAS_LAPACK_LIBS='BLAS_LAPACK_LIBS'
-
-env_lib_dirs = os.environ.get(BLAS_LAPACK_LIB_PATHS, [])
-env_libs = os.environ.get(BLAS_LAPACK_LIBS, [])
-
-if env_lib_dirs or env_libs:
-    print("using environment variables for blas/lapack libraries")
-    env_vars = {}
-    if env_lib_dirs:
-        env_vars['library_dirs'] = env_lib_dirs.split(':')
-    if env_libs:
-        env_vars['libraries'] = env_libs.split(':') 
-    install_scs(USE_64_BIT_BLAS=USE_64_BIT_BLAS, blas_info=env_vars, lapack_info={},  USE_OPENMP=USE_OPENMP, rootDir=rootDir)
-else:
-    # environment variables not set, using defaults instead
-    try:
-        print("using blas_opt / lapack_opt")
-        install_scs(USE_64_BIT_BLAS=USE_64_BIT_BLAS, blas_info=get_info('blas_opt'), lapack_info=get_info('lapack_opt'), USE_OPENMP=USE_OPENMP, rootDir=rootDir)
-    except SystemExit as e: # catch permission denied error
-        print("SystemExit")
-        print(e)
-    except:
-        # print("error:", sys.exc_info()[0])
-        print("blas_opt / lapack_opt install failed, trying blas / lapack")
-        try:
-            install_scs(USE_64_BIT_BLAS=USE_64_BIT_BLAS, blas_info=get_info('blas'), lapack_info=get_info('lapack'), USE_OPENMP=USE_OPENMP, rootDir=rootDir)
-        except:
-            install_scs(USE_64_BIT_BLAS=USE_64_BIT_BLAS, blas_info={}, lapack_info={}, USE_OPENMP=USE_OPENMP, rootDir=rootDir)
-            print("###############################################################################################")
-            print("# failed to find blas/lapack libs, SCS cannot solve SDPs but can solve LPs, SOCPs, ECPs, PCPs #")
-            print("# install blas/lapack and run this install script again to allow SCS to solve SDPs            #")
-            print("#                                                                                             #")
-            print("# scs will use environment variables BLAS_LAPACK_LIB_PATHS and BLAS_LAPACK_LIBS if set        #")
-            print("# use this to link against blas/lapack libs that scs can't find on it's own, usage ex:        #")
-            print("#        >> export BLAS_LAPACK_LIB_PATHS=/usr/lib/:/other/dir                                 #")
-            print("#        >> export BLAS_LAPACK_LIBS=blas:lapack                                               #")
-            print("#        >> python setup.py install                                                           #")
-            print("###############################################################################################")
+print('###whoooo!###')
+install_scs(USE_64_BIT_BLAS=USE_64_BIT_BLAS, blas_info=get_info('blas_opt'), lapack_info=get_info('lapack_opt'), USE_OPENMP=USE_OPENMP, rootDir=rootDir)
 
