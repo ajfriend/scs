1670173e7fbf59d4862e2f5185df361211f10192
AJ Friend <ajfriend@gmail.com>
2015-09-14 14:04:22 -0700

    refactored setup.py

--- python/setup.py
+++ python/setup.py
@@ -3,84 +3,65 @@ import os
 from setuptools import setup, Extension
 from glob import glob
 from platform import system
-from numpy import get_include
-from numpy.distutils.system_info import get_info, BlasNotFoundError 
+import numpy
+from numpy.distutils.system_info import get_info
 
-def install_scs(USE_64_BIT_BLAS, blas_info, lapack_info, USE_OPENMP, rootDir): 
-    libraries = []
-    if system() == 'Linux':
-        libraries += ['rt']
-   
-    sources = ['scsmodule.c', ] + glob(rootDir + 'src/*.c') + glob(rootDir + 'linsys/*.c')
-    include_dirs = [rootDir, rootDir + 'include', get_include(), rootDir + 'linsys']
-    
-    define_macros = [('PYTHON', None), ('DLONG', None), ('CTRLC', 1), ('COPYAMATRIX', None)]
-    # define_macros += [('EXTRAVERBOSE', 999)] # for debugging
-    extra_compile_args = ["-O3"]
-    library_dirs = []
-    extra_link_args = []
-    
-    if USE_OPENMP:
-        define_macros += [('OPENMP', None)]
-        extra_compile_args += ['-fopenmp']
-        extra_link_args += ['-lgomp']
-    
-    if USE_64_BIT_BLAS:
-        define_macros += [('BLAS64', None)]
-    
-    if blas_info or lapack_info:
-        define_macros += [('LAPACK_LIB_FOUND', None)] + blas_info.pop('define_macros', []) + lapack_info.pop('define_macros', [])
-        include_dirs += blas_info.pop('include_dirs', []) + lapack_info.pop('include_dirs', [])
-        library_dirs += blas_info.pop('library_dirs', []) + lapack_info.pop('library_dirs', [])
-        libraries += blas_info.pop('libraries', []) + lapack_info.pop('libraries', [])
-        extra_link_args += blas_info.pop('extra_link_args', []) + lapack_info.pop('extra_link_args', [])
-        extra_compile_args += blas_info.pop('extra_compile_args', []) + lapack_info.pop('extra_compile_args', [])
-    
-    _scs_direct = Extension(
-                        name='_scs_direct',
-                        sources=sources + glob(rootDir + 'linsys/direct/*.c') + glob(rootDir + 'linsys/direct/external/*.c'),
-                        define_macros=define_macros,
-                        include_dirs=include_dirs + [rootDir + 'linsys/direct/', rootDir + 'linsys/direct/external/'],
-                        library_dirs=library_dirs,
-                        libraries=libraries,
-                        extra_link_args=extra_link_args,
-                        extra_compile_args=extra_compile_args
-                        )
-    
-    _scs_indirect = Extension(
-                        name='_scs_indirect',
-                        sources=sources + glob(rootDir + 'linsys/indirect/*.c'),
-                        define_macros=define_macros + [('INDIRECT', None)],
-                        include_dirs=include_dirs + [rootDir + 'linsys/indirect/'],
-                        library_dirs=library_dirs,
-                        libraries=libraries,
-                        extra_link_args=extra_link_args,
-                        extra_compile_args=extra_compile_args
-                        )
-    setup(name='scs',
-            version='1.1.6',
-            author = 'Brendan O\'Donoghue',
-            author_email = 'bodonoghue85@gmail.com',
-            url = 'http://github.com/cvxgrp/scs',
-            description='scs: splitting conic solver',
-            py_modules=['scs'],
-            ext_modules=[_scs_direct, _scs_indirect],
-            install_requires=["numpy >= 1.7","scipy >= 0.13.2"],
-            license = "MIT",
-            long_description="Solves convex cone programs via operator splitting. Can solve: linear programs (LPs), second-order cone programs (SOCPs), semidefinite programs (SDPs), exponential cone programs (ECPs), and power cone programs (PCPs), or problems with any combination of those cones. See http://github.com/cvxgrp/scs for more details."
-            )
+import copy
 
-#####################################
+from collections import defaultdict
+
 
 # location of SCS root directory, containing 'src/' etc.
 rootDir = '../'
 
-# use 'export OMP_NUM_THREADS=16' to control num of threads (in that case, 16)
-USE_OPENMP = False
+ext = defaultdict(list)
 
-# set to true if linking against blas/lapack libraries that use longs instead of ints for indices:
-USE_64_BIT_BLAS = False
+if system() == 'Linux':
+    ext['libraries'] += ['rt']
 
-print('###whoooo!###')
-install_scs(USE_64_BIT_BLAS=USE_64_BIT_BLAS, blas_info=get_info('blas_opt'), lapack_info=get_info('lapack_opt'), USE_OPENMP=USE_OPENMP, rootDir=rootDir)
+ext['sources'] = ['scsmodule.c'] + glob(rootDir + 'src/*.c') + glob(rootDir + 'linsys/*.c')
+ext['include_dirs'] = [rootDir, rootDir + 'include', numpy.get_include(), rootDir + 'linsys']
+ext['define_macros'] = [('PYTHON', None), ('DLONG', None), ('CTRLC', 1), ('COPYAMATRIX', None)]
+# define_macros += [('EXTRAVERBOSE', 999)] # for debugging
+ext['extra_compile_args'] = ["-O3"]
+
+blas_info=get_info('blas_opt')
+lapack_info=get_info('lapack_opt')
+
+if blas_info or lapack_info:
+    ext['define_macros'] += [('LAPACK_LIB_FOUND', None)] + blas_info.pop('define_macros', []) + lapack_info.pop('define_macros', [])
+    ext['include_dirs'] += blas_info.pop('include_dirs', []) + lapack_info.pop('include_dirs', [])
+    ext['library_dirs'] += blas_info.pop('library_dirs', []) + lapack_info.pop('library_dirs', [])
+    ext['libraries'] += blas_info.pop('libraries', []) + lapack_info.pop('libraries', [])
+    ext['extra_link_args'] += blas_info.pop('extra_link_args', []) + lapack_info.pop('extra_link_args', [])
+    ext['extra_compile_args'] += blas_info.pop('extra_compile_args', []) + lapack_info.pop('extra_compile_args', [])
+
+
+# deep copy so that the dictionaries do not point to the same list objects
+ext_direct = copy.deepcopy(ext)
+ext_direct['name'] = '_scs_direct'
+ext_direct['sources'] += glob(rootDir + 'linsys/direct/*.c') + glob(rootDir + 'linsys/direct/external/*.c')
+ext_direct['include_dirs'] += [rootDir + 'linsys/direct/', rootDir + 'linsys/direct/external/']
+_scs_direct = Extension(**ext_direct)
+
+ext_indirect = copy.deepcopy(ext)
+ext_indirect['name'] = '_scs_indirect'
+ext_indirect['sources'] += glob(rootDir + 'linsys/indirect/*.c')
+ext_indirect['define_macros'] += [('INDIRECT', None)]
+ext_indirect['include_dirs'] += [rootDir + 'linsys/indirect/']
+_scs_indirect = Extension(**ext_indirect)
+
+
+setup(name='scs',
+        version='1.1.6',
+        author = 'Brendan O\'Donoghue',
+        author_email = 'bodonoghue85@gmail.com',
+        url = 'http://github.com/cvxgrp/scs',
+        description='scs: splitting conic solver',
+        py_modules=['scs'],
+        ext_modules=[_scs_direct, _scs_indirect],
+        install_requires=["numpy >= 1.7","scipy >= 0.13.2"],
+        license = "MIT",
+        long_description="Solves convex cone programs via operator splitting. Can solve: linear programs (LPs), second-order cone programs (SOCPs), semidefinite programs (SDPs), exponential cone programs (ECPs), and power cone programs (PCPs), or problems with any combination of those cones. See http://github.com/cvxgrp/scs for more details."
+        )
 
